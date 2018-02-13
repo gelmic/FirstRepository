17b4aaa7488a7f30bdbe69a5d822103b32b681d6
Ronald Lee <Ronald Lee@ISD-PF01C1P0.gdcusaadc.com>
2018-02-13 20:37:59 +0800

    First Commit

---
+++ GitHubSample/src/com/ronn/SampleClass.java
@@ -0,0 +1,14 @@
+package com.ronn;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		SampleClass sampleClass = new SampleClass();
+		sampleClass.displayMessage("Sample Message");
+	}
+	
+	public void displayMessage(String message) {
+		System.out.println(message);
+	}
+
+}
